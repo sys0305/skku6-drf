77808d5ab5aed1ad406b45771e10c492219ee127
skku24\user <ys@skkukdp.re.kr>
2024-09-09 15:48:12 +0900

    student serializer

--- study/serializers.py
+++ study/serializers.py
@@ -2,11 +2,23 @@ from rest_framework import serializers
 from .models import Student, Score
 
 
+class ScoreListSerializer(serializers.RelatedField):
+
+    def to_representation(self, value):
+        return {
+            "math": value.math,
+            "english": value.english,
+            "science": value.science
+        }
+
+
 class StudentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Student
-        fields = ["name", "address", "email"]
+        fields = ["name", "address", "email", "score_set"]
+
+    score_set = ScoreListSerializer(many=True, read_only=True)
 
 
 # Serializer도 추가해주세요.
@@ -16,3 +28,5 @@ class ScoreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Score
         fields = ["student", "english", "math", "science", "exam_date"]
+
+    # student = StudentSerializer()
--- study/views.py
+++ study/views.py
@@ -6,13 +6,31 @@ from rest_framework.views import Response
 from .models import Student, Score
 from .serializers import StudentSerializer, ScoreSerializer
 
+# @api_view(["GET"])
+# def StudentView(request):
+#     # qs = Student.objects.all()
+#     qs = Student.objects.prefetch_related("score_set").all()
 
-@api_view(["GET"])
+#     # ListSerializer if many=True
+#     serializer = StudentSerializer(qs, many=True)
+#     return Response(serializer.data)
+
+from rest_framework import status
+
+
+@api_view(["GET", "POST"])
 def StudentView(request):
-    qs = Student.objects.all()
-    # ListSerializer if many=True
-    serializer = StudentSerializer(qs, many=True)
-    return Response(serializer.data)
+    if request.method == "GET":
+        qs = Student.objects.prefetch_related("score_set").all()
+        # ListSerializer if many=True
+        serializer = StudentSerializer(qs, many=True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(["GET"])
